8a3778273565d9c04093d25d4120dabf9d2cd161
Jacob Trock <jgh@geocrypt.me>
2024-09-17 10:52:35 -0400

    completed

--- src/models/calculator.model.spec.ts
+++ src/models/calculator.model.spec.ts
@@ -1,6 +1,7 @@
 
 import { CalculatorModel } from './calculator.model';
 import { ICalculatorModel } from '../interfaces/calculator-model.interface';
+import { NumericKeys } from '../enums/numeric-keys.enum';
 
 describe('CalculatorModel', (): void => {
 
@@ -9,6 +10,51 @@ describe('CalculatorModel', (): void => {
     const calculator: ICalculatorModel = new CalculatorModel();
 
     expect(calculator).toBeDefined();
+
+    // expect submethods pressNumericKey, pressOperatorKey, pressActionKey, display to be defined
+    expect(calculator.pressNumericKey).toBeDefined();
+    expect(calculator.pressOperatorKey).toBeDefined();
+    expect(calculator.pressActionKey).toBeDefined();
+    expect(calculator.display).toBeDefined();
+
+  });
+
+  it('should have an empty display on init', (): void => {
+
+    // Assemble
+    const calculator: ICalculatorModel = new CalculatorModel();
+
+    // Act
+    const displayValue: string = calculator.display();
+
+    // Assert
+    expect(displayValue).toEqual('');
+
+  });
+
+  it('should display `1` when the `1` key is pressed', (): void => {
+
+    // Assemble
+    const calculator: ICalculatorModel = new CalculatorModel();
+
+    // Act
+    calculator.pressNumericKey(NumericKeys.ONE);
+    const displayValue: string = calculator.display();
+
+    // Assert
+    expect(displayValue).toEqual('1');
+
+  });
+
+  it('should display `98` when the `9` key is pressed followed by the `8` key', (): void => {
+
+    const calculator: ICalculatorModel = new CalculatorModel();
+
+    calculator.pressNumericKey(NumericKeys.NINE);
+    calculator.pressNumericKey(NumericKeys.EIGHT);
+    const displayValue: string = calculator.display();
+
+    expect(displayValue).toEqual('98');
 
   });
 
--- src/models/calculator.model.ts
+++ src/models/calculator.model.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-eval */
 
 import { ActionKeys } from '../enums/action-keys.enum';
 import { NumericKeys } from '../enums/numeric-keys.enum';
@@ -5,21 +6,53 @@ import { OperatorKeys } from '../enums/operator-keys.enum';
 import { ICalculatorModel } from '../interfaces/calculator-model.interface';
 
 export class CalculatorModel implements ICalculatorModel {
+  private displayValue: string = '';
 
   public pressNumericKey(key: NumericKeys): void {
-    throw new Error('Method not implemented.');
+    this.displayValue += key;
   }
 
   public pressOperatorKey(key: OperatorKeys): void {
-    throw new Error('Method not implemented.');
+    this.displayValue += key;
   }
 
   public pressActionKey(key: ActionKeys): void {
-    throw new Error('Method not implemented.');
+    if (key === ActionKeys.CLEAR) {
+      this.displayValue = '';
+    } else if (key === ActionKeys.EQUALS) {
+      // perform string math(eval not allowed)
+      const vals: string[] = this.displayValue.split('');
+      const operator: string = vals.find((val: string): boolean => val === '+' || val === '-' || val === '*' || val === '/');
+      const operatorIndex: number = vals.indexOf(operator);
+      const leftVal: number = parseFloat(vals.slice(0, operatorIndex).join(''));
+      const rightVal: number = parseFloat(vals.slice(operatorIndex + 1).join(''));
+      let result: number;
+      switch (operator) {
+        case '+':
+          result = leftVal + rightVal;
+          break;
+        case '-':
+          result = leftVal - rightVal;
+          break;
+        case '*':
+          result = leftVal * rightVal;
+          break;
+        case '/':
+          result = leftVal / rightVal;
+          break;
+        default:
+          throw new Error('Invalid operator');
+      }
+      this.displayValue = result.toString();
+    } if (key === ActionKeys.DOT) {
+      this.displayValue += '.';
+    } else {
+      throw new Error('Invalid action key');
+    }
   }
 
   public display(): string {
-    throw new Error('Method not implemented.');
+    return this.displayValue;
   }
 
 }
